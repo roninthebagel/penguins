d3e469384e2c1b4f46f6b673d8970238353cc21c
roninthebagel <ronin@run2online.com>
2025-02-11 12:50:39 +0000

    added using dplyr::select()

--- scripts/data_tidying.R
+++ scripts/data_tidying.R
@@ -1,9 +1,13 @@
 # reading .csv file
 data <- read_csv("data/messy_penguins.csv")
 
+#__________________________----
+
 # using library(here) - builds a path to a select directory (i.e. dataset)
 library(here)
 raw_data <- read_csv(here("data", "messy_penguins.csv"))
+
+#__________________________----
 
 # using glimpse()
 # Quickly inspect the structure of the dataset, including column names and types
@@ -18,6 +22,8 @@ summary(raw_data)
 # including data types, missing values, and descriptive statistics
 skim(raw_data)
 
+#__________________________----
+
 # using colnames()
 # tells us the names of all columns
 colnames(raw_data)
@@ -27,4 +33,16 @@ penguins_clean_names <- janitor::clean_names(raw_data)
 
 # view new column names
 colnames(penguins_clean_names) 
+
+#__________________________----
+
+# using dplyr::select()
+# tells us information with only the mentioned variables
+select(
+  .data = penguins_clean_names,
+  species, sex, flipper_length_mm, body_mass_g)
+
+# or using dplyr::select() to eliminate columns
+select(.data = penguins_clean_names,
+       -study_name, -sample_number)
 
