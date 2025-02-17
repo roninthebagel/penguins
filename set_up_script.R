0ca9c3125445bfae354699bb1e953a1fb9b89a63
roninthebagel <ronin@run2online.com>
2025-02-17 14:05:01 +0000

    initial commit

---
+++ set_up_script.R
@@ -0,0 +1,20 @@
+# Required packages
+library(here)
+library(tidyverse)
+library(GGally)
+library(skimr)
+library(naniar)
+library(mice)
+library(ggridges)
+library(usethis)
+library(gitcreds)
+library(janitor)
+
+# github credentials
+library(usethis) 
+use_git_config(user.name  = "roninthebagel",
+               user.email = "ronin@run2online.com")
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
